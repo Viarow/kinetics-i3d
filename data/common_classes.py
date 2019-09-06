649b44337d41b4729636e2e208ccb84b46c96478
Viarow <universe37@outlook.com>
2019-09-06 21:10:51 +0800

    common_classes

---
+++ data/common_classes.py
@@ -0,0 +1,15 @@
+
+"""
+Record the common classes between Kinetics400 and (UCF101, HMDB51, Moments In Time)
+"""
+
+Kinetics400_UCF101 = [('Archery', 5), ('BenchPress', 19), ('Biking', 22), ('BlowingCandles', 27),
+                      ('Bowling', 31), ('BrushingTeeth', 37), ('RopeClimbing', 66), ('BabyCrawling', 77),
+                      ('CliffDiving', 93), ('HammerThrow', 148), ('HighJump', 154), ('HulaHoop', 159),
+                      ('JavelinThrow', 166), ('LongJump', 182), ('Lunges', 183), ('MoppingFloor', 198),
+                      ('PlayingCello', 223), ('Drumming', 230), ('PlayingFlute',231), ('PlayingGuitar', 232),
+                      ('PlayingPiano', 241), ('PlayingViolin', 250), ('PoleVault', 253), ('PullUps', 255),
+                      ('PushUps', 260), ('Biking', 267), ('HorseRiding', 273), ('Shotput', 298),
+                      ('SkateBoarding', 306), ('SkyDiving', 312), ('BreastStroke', 340), ('TaiChi', 346),
+                      ('ThrowDiscus', 358), ('WalkWithDog', 378)]
+
